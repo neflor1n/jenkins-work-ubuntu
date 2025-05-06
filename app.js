795e4c87b45d889bdf0a82588aed3bd6c1b3600b
neflor1n <153904907+neflor1n@users.noreply.github.com>
2025-05-06 16:49:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/movie', (req, res) => {
-  res.send('Мой любимый фильм: Побег из Шоушенка');
+app.get('/', (req, res) => {
+  res.send('Minu lemmikmagustoit: Napoleonikook');
 });
 
 app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
+  console.log(`Server töötab http://localhost:${port}`);
 });
